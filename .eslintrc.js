b81da6a73a559ec4e226734cccb25fb88c6c461c
Wayne Chiu <chaoweichiu@gmail.com>
2018-10-14 15:52:54 +0800

    add flow type

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-    "extends": "airbnb",
+    "extends": [
+        "airbnb",
+        "plugin:flowtype/recommended",
+    ],
     "ecmaFeatures": {
         "modules": true,
-        "module":  true
+        "module":  true,
     },
+    "plugins": [
+        "flowtype",
+    ],
     "rules": {
         "semi": [1, 'never'],
         "react/jsx-filename-extension": [1, { "extensions": [".jsx", ".js"] }],
