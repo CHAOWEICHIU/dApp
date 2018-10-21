71718249eaa764081624463abf2af05a967c0928
Wayne Chiu <chaoweichiu@gmail.com>
2018-10-21 09:28:11 +0800

    add complie script

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,12 +1,48 @@
 const solc = require('solc')
 const path = require('path')
 const fs = require('fs')
+const mapKeys = require('lodash/mapKeys')
+const forEach = require('lodash/forEach')
+const { systemLogger } = require('../utils/logger')
+
+const contracts = [
+  {
+    contractFilePath: path.resolve(process.env.PWD, 'contracts', 'Game', 'PlayerBook.sol'),
+  },
+  {
+    contractFilePath: path.resolve(process.env.PWD, 'contracts', 'Game', 'Game.sol'),
+  },
+]
 
 
-const compile = ({ contractPathFromRoot }) => {
-  const inboxPath = path.resolve(process.env.PWD, contractPathFromRoot)
-  const source = fs.readFileSync(inboxPath, 'utf8')
+const compile = ({ filePath }) => {
+  const source = fs.readFileSync(filePath, 'utf8')
   return solc.compile(source, 1)
 }
 
-module.exports = compile
+const startCompile = (async () => {
+  const compliedContracts = await contracts
+    .map(contract => compile({ filePath: contract.contractFilePath }))
+
+  forEach(compliedContracts, (compliedContract) => {
+    mapKeys(compliedContract.contracts, (value, key) => {
+      systemLogger({ message: `${key} complied` })
+
+      fs.writeFileSync(
+        path.resolve(process.env.PWD, '.contracts', `${key.slice(1, key.length)}.js`),
+        `
+const contractInterface = ${value.interface}
+
+const contractInterfaceBytecode = '${value.bytecode}'
+
+module.exports = {
+  contractInterface,
+  contractInterfaceBytecode,
+}
+        `,
+      )
+    })
+  })
+})
+
+startCompile()
