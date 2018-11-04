42b1fe4c3e2252efeb402b582c67cdc62c089049
Wayne Chiu <chaoweichiu@gmail.com>
2018-11-04 09:27:06 +0800

    update mushroom contract

--- contracts/Mushroom.sol
+++ contracts/Mushroom.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.23;
 
 contract Mushroom {
     using LedgerSystem for LedgerSystem.Data;
@@ -6,7 +6,7 @@ contract Mushroom {
     
     address public owner;
     mapping(address => User) users;
-    address[] public all_users;
+    address[] all_users;
     MushroomGameSystem.Data mushroomGames;
     uint256 public mushroom_eth_rate;
     
@@ -21,8 +21,12 @@ contract Mushroom {
         mushroom_eth_rate = 1E14;
     }
     
+    function getAllUsers()public view returns(address[]) {
+        return all_users;
+    }
+    
     function registerUser() public {
-        require(users[msg.sender].registerBlockheight == 0);
+        require(users[msg.sender].registerBlockheight == 0, "you have registed");
         all_users.push(msg.sender);
         users[msg.sender] = User({
             registerBlockheight: block.number,
@@ -31,7 +35,7 @@ contract Mushroom {
         });
     }
     
-    function getGuCoin(address _user) public view returns (uint256) {
+    function getGuCoin(address _user) internal view returns (uint256) {
         User storage user = users[_user];
         if(user.registerBlockheight>0) {
             uint256 base = (block.number - user.registerBlockheight);
@@ -40,7 +44,7 @@ contract Mushroom {
     }
     
     function buyGuCoin() public payable {
-        require(msg.value / mushroom_eth_rate > 1);
+        require(msg.value / mushroom_eth_rate > 1, "Not enough money");
         User storage _u = users[msg.sender];
         _u.ledger.operation(msg.value / mushroom_eth_rate, true, "buy Gu Coin");
     }
@@ -53,8 +57,34 @@ contract Mushroom {
         return mushroomGames.joinGame(game_id, msg.sender);
     }
     
-    function getMushroom(uint256 game_index, uint8 team_index) public view returns (uint256){
-        return mushroomGames.calcMushroom(game_index, team_index, block.number);
+    function gameWithTeam(uint256 game_index, uint8 team_index) public view returns (
+        uint256,    // score
+        address[],  // team_members
+        uint8[]    // levels
+        ){
+        uint256 score = mushroomGames.calcMushroom(game_index, team_index, block.number);
+
+        (address[] memory addresses, uint8[] memory levels) = mushroomGames.getTeamMember(game_index, team_index);
+        return (score, addresses, levels);
+    }
+    
+    function getBankerGame(address _addr) view public returns (bool, uint256) {
+        return mushroomGames.getBankerGame(_addr);
+    }
+    
+    function getGameInfo(uint256 game_index)
+        public 
+        view 
+        returns (
+            string,     // pending || started || aborted || ended
+            address[],  // participants
+            uint256,    // created_at_block
+            uint256,    // game_start_at
+            uint256,    // pending_block
+            uint8      // team count
+        )
+    {
+        return mushroomGames.getGameInfo(game_index);
     }
 }
 
@@ -100,21 +130,77 @@ library MushroomGameSystem {
         uint256 block;
     }
     
+    function getTeamMember(Data storage self, uint256 game_index, uint256 team_index) internal view returns (address[], uint8[]) {
+        MushroomGame storage _m = self.mushroomGames[game_index];
+        address[] memory addr = new address[](_m.gamer_count / _m.team_count);
+        uint8[] memory level = new uint8[](_m.gamer_count / _m.team_count);
+        uint8 idx = 0;
+        for(uint8 i=0 ;i< _m.gamer_count; i++) {
+            if(_m.team_member[_m.gamer[i]] == team_index) {
+                addr[idx] = _m.gamer[i];
+                level[idx]=_m.levels[_m.gamer[i]];
+                idx += 1;
+            }
+        }
+        return (addr, level);
+    }
+    
+    function getGameInfo(Data storage self, uint256 game_index) internal view returns (
+            string,     // pending || started || aborted || ended
+            address[],  // participants
+            uint256,    // created_at_block
+            uint256,    // game_start_at
+            uint256,    // pending_block
+            uint8      // team count
+        ){
+        MushroomGame storage _m = self.mushroomGames[game_index];
+        string memory status;
+        address[] memory addr;
+        if(_m.game_start_at == 0) {
+            if(block.number - _m.created_at_block >= _m.pending_block) {
+                status = "aborted";
+            } else {
+                status = "pending";
+            }
+        } else {
+            if(block.number - _m.game_start_at >= _m.gaming_block) {
+                status = "ended";
+            } else {
+                status = "started";
+            }
+        } 
+        for(uint8 i=0;i<_m.gamer_count;i++) {
+            addr[i] = _m.gamer[i];
+        } 
+        return (status, addr, _m.created_at_block, _m.pending_block, _m.game_start_at, _m.team_count);
+    }
+    
+    function getBankerGame(Data storage self, address _banker) internal view returns (bool, uint256) {
+        for(uint256 i=0;i<self.index;i++) {
+            if(!isEnd(self, i) && self.mushroomGames[i].banker == _banker) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
 
     
     function calcMushroom(Data storage self, uint256 game_index, uint8 team_index, uint256 target_block) view internal returns (uint256) {
         MushroomGame storage _m = self.mushroomGames[game_index];
+        //uint248 team_members = _m.gamer_count / _m.team_count;
         uint256 total_gugu = 0;
         uint256 end_block = 0;
         if(_m.game_start_at + _m.gaming_block <= target_block) {
             end_block = _m.gaming_block + _m.game_start_at;
         } else {
-            end_block = target_block - _m.game_start_at;
+            end_block = target_block;
+        }
+        for(uint8 x=0;x<_m.gamer_count/_m.team_count; x++) {
+            total_gugu += (end_block - _m.game_start_at) * 3;
         }
         
-        total_gugu += (end_block - _m.game_start_at) * 3;
         for(uint256 i=0; i<_m.ledger_count; i++) {
-            GuGuLedger storage _l = _m.ledgers[i];
+            GuGuLedger memory _l = _m.ledgers[i];
             if(_l.team_index != team_index){
                 continue;
             }
@@ -136,8 +222,7 @@ library MushroomGameSystem {
     }
     
     function newGame(
-        Data storage self,
-        address _banker,
+        Data storage self, address _banker,
         uint256 _deposit, uint256 _pending_block,
         uint256 _gaming_block) internal returns (uint256) {
         self.mushroomGames[self.index] = MushroomGame({
@@ -155,11 +240,9 @@ library MushroomGameSystem {
         self.index += 1;
         return self.index - 1 ;
     }
-
+    
     function newGameItem(
-        Data storage self,
-        uint256 game_index,
-        string _name,
+        Data storage self, uint256 game_index, string _name,
         uint32 _price, uint32 _duration, uint8 _production_rate,
         uint8 _mushroom_rate, bool _unique) internal {
         MushroomGame storage _m = self.mushroomGames[game_index];
@@ -202,12 +285,12 @@ library MushroomGameSystem {
         if(_m.game_start_at != 0) {
             return false;
         }
-        if(_m.gamer_count < 11) {
+        if(_m.gamer_count < 2) {
             _m.gamer[_m.gamer_count] = user;
             _m.levels[user] = 1;
             _m.gamer_count += 1;
         } 
-        if(_m.gamer_count >= 11) {
+        if(_m.gamer_count >= 2) {
             randonGamer(self, game_index);
             _m.game_start_at = block.number;
         }
@@ -216,7 +299,7 @@ library MushroomGameSystem {
     function randonGamer(Data storage self, uint256 game_index) internal {
         MushroomGame storage _m = self.mushroomGames[game_index];
         for(uint8 i=0; i<_m.gamer_count; i++) {
-            _m.team_member[_m.gamer[i]] = (i % 2) + 1; 
+            _m.team_member[_m.gamer[i]] = (i+1) % 2; 
         }
         return;
     }
@@ -237,8 +320,7 @@ library LedgerSystem {
     
     function operation(
         Data storage self, uint256 _amount,
-        bool _positive, string _desc
-    ) internal {
+        bool _positive, string _desc) internal {
         self.ledgers[self.index] = Ledger({
             amount: _amount,
             positive: _positive,
